517d42c59c07998a5f79489780145c3e6e72a360
Eric Paul <eric.paul@me.com>
2018-04-25 14:31:45 -0400

    Code changes in index.html and app.js

---
+++ js/app.js
@@ -0,0 +1,39 @@
+//Model for App
+
+//Location listings and
+var locations = [
+  {
+    name: "Amsterdam Ale House",
+    lat: 40.781422,
+    lng: -73.979954,
+    id: "49ef2aabf964a52083681fe3"
+  },
+  {
+    name: "Rudy's",
+    lat: 40.760029,
+    lng: -73.991766,
+    id: "3fd66200f964a5208de81ee3"
+  },
+  {
+    name: "Stumptown Coffee Roasters",
+    lat: 40.745632,,
+    lng: -73.988052,
+    id: "44aa52d50f964a520834720e3"
+  },
+  {
+    name: "Eataly Flatiorn",
+    lat: 40.742164,
+    lng: -73.989894,
+    id: "4c5ef77bfff99c74eda954d3"
+  },
+  {
+    name: "Cleopatra's Needle",
+    lat: 40.792474,
+    lng: -73.973683,
+    id: "3fd66200f964a52055e81ee3"
+  },
+]
+
+// Client ID and Secret for Foursquare
+var CLIENT_ID_Foursquare = '?client_id=PGRNG1GBR34H5Y025X0EWBXDPKDA2KIXVL2L1VEU4OIJDFUR';
+var CLIENT_SECRET_Foursquare = '&client_secret=X4AIVIWMJLFIEMBMSAXC42WSN0PNXV5QCOCL3EMQOWOI3LO3';
