1f5c3ba97ccf84d36d10a9a95895aae38c9ecca1
Eric Paul <eric.paul@me.com>
2018-04-27 11:35:08 -0400

    Removal of list data

--- js/app.js
+++ js/app.js
@@ -7,39 +7,6 @@ var marker;
 var CLIENT_ID = "PGRNG1GBR34H5Y025X0EWBXDPKDA2KIXVL2L1VEU4OIJDFUR";
 var CLIENT_SECRET = "X4AIVIWMJLFIEMBMSAXC42WSN0PNXV5QCOCL3EMQOWOI3LO3";
 
-//Location array
-var locations = [{
-    name: "Amsterdam Ale House",
-    lat: 40.781422,
-    lng: -73.979954,
-    id: "49ef2aabf964a52083681fe3"
-  },
-  {
-    name: "Rudy's",
-    lat: 40.760029,
-    lng: -73.991766,
-    id: "3fd66200f964a5208de81ee3"
-  },
-  {
-    name: "Stumptown Coffee Roasters",
-    lat: 40.745632,
-    lng: -73.988052,
-    id: "4aa52d50f964a520834720e3"
-  },
-  {
-    name: "Eataly Flatiorn",
-    lat: 40.742164,
-    lng: -73.989894,
-    id: "4c5ef77bfff99c74eda954d3"
-  },
-  {
-    name: "Cleopatra's Needle",
-    lat: 40.792474,
-    lng: -73.973683,
-    id: "3fd66200f964a52055e81ee3"
-  },
-]
-
 //Setting up map view
 var map;
 
@@ -81,6 +48,7 @@ function initMap() {
           var result = data.response.venue;
           location.address = result.location.address;
           location.url = result.canonicalUrl;
+          location.i
 
           // Fallback for locations that don't have rating set in FourSquare
           if (result.rating !== undefined) {
